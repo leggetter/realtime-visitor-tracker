3bbb5c164976113bba47e7b425e7d59a667703ea
Phil Leggetter <phil@leggetter.co.uk>
2012-04-17 14:40:15 +0100

    initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+  require_once('config.php');
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Realtime Visitor Tracker Example</title>
+  <meta name="description" content="Realtime Visitor Tracker Example" />
+  <link rel="stylesheet" href="css/styles.css?v=1.0" />
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+  
+</body>
+</html>
